cfbac88bbdb88e44c440b3c64852b8266a9e28cd
AymericM <aymeric.moehn@gmail.com>
2019-11-06 10:48:13 +0100

    gtm update

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import TagManager from 'react-gtm-module'
 import Routes from './routes/main';
-import history from './routes/history'
 import { Router } from 'react-router-dom';
-import registerServiceWorker from './registerServiceWorker';
-import './styles/global'
+import history from './routes/history'
 import { MainProvider } from './stores/MainStore';
+import registerServiceWorker from './registerServiceWorker';
+import * as pjson from '../package.json'
+import './styles/global'
+
+const tagManagerArgs = {
+    gtmId: 'GTM-MZQTPRC',
+    dataLayer: {
+        version: pjson.version
+    }
+}
+ 
+TagManager.initialize(tagManagerArgs)
 
 ReactDOM.render(
     <Router history={history}>
